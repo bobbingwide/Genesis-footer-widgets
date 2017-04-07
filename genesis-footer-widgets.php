8ca0b197ef7cb1e10be9577f08ca12bdfd020308
Herb Miller <herb@bobbingwide.com>
2017-04-07 16:57:27 +0100

    Fixes #6 - correct CSS for <= 960px

--- genesis-footer-widgets.php
+++ genesis-footer-widgets.php
@@ -1,17 +1,17 @@
 <?php 
 /*
 Plugin Name: Genesis Footer Widgets
-Plugin URI: http://www.oik-plugins.com/oik-plugins/genesis-footer-widgets
+Plugin URI: https://www.oik-plugins.com/oik-plugins/genesis-footer-widgets
 Description: Responsive Footer Widgets for the Genesis theme framework 
-Version: 0.0.1
+Version: 0.0.2
 Author: bobbingwide
-Author URI: http://www.oik-plugins.com/author/bobbingwide
+Author URI: https://www.oik-plugins.com/author/bobbingwide
 Text Domain: genesis-footer-widgets
 Domain Path: /languages/
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
-    Copyright 2016 Bobbing Wide (email : herb@bobbingwide.com )
+    Copyright 2016, 2017 Bobbing Wide (email : herb@bobbingwide.com )
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License version 2,
